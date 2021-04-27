595590d26fd86978166cf88d397eb66e4a5b6a07
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-04-27 17:41:16 +0400

    Add functionality to load more data by scrolling down

--- lib/view/screens/pages/home/home.dart
+++ lib/view/screens/pages/home/home.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
+import 'package:anon/core/model/post.dart';
 import 'package:anon/view/screens/pages/home/create_post.dart';
 import 'package:anon/view/widgets/utils/route_with_transition.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,11 +26,34 @@ class HomeState extends AnonState<Home> {
   UserserviceBloc _userserviceBloc;
 
   final _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+  final _scrollController = ScrollController();
+
+  int currentMaxPostLength = 10;
+
+  List<PostModel> posts;
 
   @override
   void initState() {
     super.initState();
     _userserviceBloc = BlocProvider.of<UserserviceBloc>(context);
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getList();
+      }
+    });
+  }
+
+  void _getList() {
+    if (_userserviceBloc.state.postModelList.length != posts.length) {
+      posts = _userserviceBloc.state.postModelList
+          .getRange(0, currentMaxPostLength + 10)
+          .toList();
+      print("Yihuuuuuu");
+      setState(() {
+        currentMaxPostLength = currentMaxPostLength + 10;
+      });
+    }
   }
 
   Future<void> _refresh() async {
@@ -41,48 +66,49 @@ class HomeState extends AnonState<Home> {
   Widget build(BuildContext context) {
     return BlocBuilder<UserserviceBloc, UserServiceState>(
       builder: (context, state) {
+        posts = state.postModelList.take(currentMaxPostLength).toList();
         return Scaffold(
           appBar: _appbar(context),
-          body: _buildRefreshableBody(),
+          body: posts.length == 0
+              ? Container()
+              // ? Center(child: CupertinoActivityIndicator(radius: 25))
+              : _buildRefreshableBody(state),
         );
       },
     );
   }
 
-  RefreshIndicator _buildRefreshableBody() {
+  RefreshIndicator _buildRefreshableBody(UserServiceState state) {
     return RefreshIndicator(
       backgroundColor: Colors.black,
       color: Colors.white,
       key: _refreshIndicatorKey,
       onRefresh: _refresh,
-      child: BlocBuilder<UserserviceBloc, UserServiceState>(
-        builder: (context, state) {
-          if (state.event == UserServiceEvents.getAllError) {
-            return errorModal();
-          }
-          return buildContentList(state);
-        },
-      ),
+      child: (state.event == UserServiceEvents.getAllError)
+          ? errorModal()
+          : buildContentList(state),
     );
   }
 
   ListView buildContentList(UserServiceState state) {
-    return ListView(
+    return ListView.builder(
       shrinkWrap: true,
-      children: [
-        SizedBox(height: 20),
-        Column(
-          children: state.postModelList
-              .map(
-                (postModel) => PostCardWidget(
-                  postModel: postModel,
-                  onTap: () {},
-                  onViewCommentsTap: () {},
-                ),
-              )
-              .toList(),
-        ),
-      ],
+      itemCount: currentMaxPostLength,
+      controller: _scrollController,
+      itemBuilder: (context, index) {
+        if (index == posts.length) {
+          return CupertinoActivityIndicator(radius: 15);
+        }
+        return Column(
+          children: [
+            PostCardWidget(
+              postModel: posts[index],
+              onTap: () {},
+              onViewCommentsTap: () {},
+            ),
+          ],
+        );
+      },
     );
   }
 
