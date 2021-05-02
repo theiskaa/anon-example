a637dae3027dc456f54e8e75f13233e5c73b527e
Ismael Shakverdiev <st.bahahd@gmail.com>
2021-05-02 15:03:44 +0400

    Add comment model and comment model test

---
+++ lib/core/model/comment.dart
@@ -0,0 +1,27 @@
+class CommentModel {
+  final String title;
+  final String description;
+
+  const CommentModel({
+    this.title,
+    this.description,
+  });
+
+  CommentModel copyWith({
+    String title,
+    String description,
+  }) =>
+      CommentModel(
+        title: title ?? this.title,
+        description: description ?? this.description,
+      );
+
+  CommentModel.fromJson(Map<String, Object> json)
+      : this.title = json['title'],
+        this.description = json['description'];
+
+  Map<String, dynamic> toJson({CommentModel commentModel}) => {
+        'title': title ?? this.title,
+        'description': description ?? this.description,
+      };
+}
